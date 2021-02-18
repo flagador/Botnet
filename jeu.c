21596933697eaf0dc7cdbcc344f47f7e4e9b3f61
jlebouc35 <julian.lebouc@etudiant.univ-rennes1.fr>
2021-02-18 15:21:14 +0100

    Debut jeu avec fonction buy_upgrade [BUGGÉ]

---
+++ jeu.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "virus.h"
+#include "upgrade.h"
+#include "jeu.h"
+
+
+
+extern jeu_t * jeu_create(virus_t *virus, float btc){ //cr�� un jeu
+    jeu_t * jeu = malloc(sizeof(jeu_t));
+
+    jeu->virus=virus;
+
+    jeu->btc=btc;
+
+    return(jeu);
+}
+
+void buy_upgrade(jeu_t *jeu, upgrade_t *upgrade){ //achat d'un upgrade
+
+    if(jeu->btc>upgrade->price){
+        jeu->btc=jeu->btc-upgrade->price; //enleve le prix de l'upgrade � notre argent
+
+        *jeu->virus->spreading_rate+=upgrade->spreading_rate;
+
+        *jeu->virus->research_rate+=upgrade->research_rate;
+    } else {
+        printf("Pas assez d'argent \n");
+
+    } 
+}
+
+int main(){
+    virus_t * virus = virus_create("kaboub", 0.5, 0.5);
+    virus_display(virus);
+
+    upgrade_t * upgrade = upgrade_create("phishing", 30.5, 0.2, 0.2);
+    upgrade_display(upgrade);
+
+    jeu_t *jeu= jeu_create(virus, 200.5);
+    virus_display(jeu->virus);
+
+    printf("Thunes : %f \n", jeu->btc);
+    buy_upgrade(jeu, upgrade);
+    virus_display(virus);
+    printf("Thunes : %f \n", jeu->btc);
+
+
+    return 0;
+}
---
+++ jeu.h
@@ -0,0 +1,16 @@
+#ifndef _JEU_H_
+#define _JEU_H_
+#include "virus.h"
+#include "upgrade.h"
+
+
+
+typedef struct
+{
+	virus_t *virus;
+    float btc;
+} jeu_t ;
+
+extern jeu_t * jeu_create(virus_t *virus, float btc);
+
+#endif
