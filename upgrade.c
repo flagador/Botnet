ac346fca6dc6b4d0f5417aa56863360d1985d4f8
jlebouc35 <julian.lebouc@etudiant.univ-rennes1.fr>
2021-02-18 14:10:07 +0100

    correction structure upgrade

--- upgrade.c
+++ upgrade.c
@@ -4,46 +4,51 @@
 #include "upgrade.h"
 
 
-extern upgrade_t * upgrade_create(char name[], float price, float spr_rate, float rsch_rate){ //créé un upgrade
+extern upgrade_t * upgrade_create(char name[], float price, float spr_rate, float rsch_rate){ //crï¿½ï¿½ un upgrade
     upgrade_t * upgrade = malloc(sizeof(upgrade_t));
 
     upgrade->name=name;
 
-    upgrade->price=&price;
+    upgrade->price=price;
 
-    upgrade->spreading_rate=&spr_rate;
+    upgrade->spreading_rate=spr_rate;
 
-    upgrade->research_rate=&rsch_rate;
+    upgrade->research_rate=rsch_rate;
     return(upgrade);
 }
 
-extern void *upgrade_edit_price(upgrade_t * upgrade, float price){ //modifie le prix du upgrade passé en parametre
-    upgrade->price=&price;
+extern void *upgrade_edit_price(upgrade_t * upgrade, float price){ //modifie le prix du upgrade passï¿½ en parametre
+    upgrade->price=price;
 }
 
-extern void *upgrade_edit_rsch_rate(upgrade_t * upgrade, float rsch_rate){ //modifie le taux de recherche du upgrade passé en parametre
-    upgrade->research_rate=&rsch_rate;
+extern void *upgrade_edit_rsch_rate(upgrade_t * upgrade, float rsch_rate){ //modifie le taux de recherche du upgrade passï¿½ en parametre
+    upgrade->research_rate=rsch_rate;
 }
 
-extern void *upgrade_edit_spr_rate(upgrade_t * upgrade, float spr_rate){ //modifie le taux de propagation du upgrade passé en parametre
-    upgrade->spreading_rate=&spr_rate;
+extern void *upgrade_edit_spr_rate(upgrade_t * upgrade, float spr_rate){ //modifie le taux de propagation du upgrade passï¿½ en parametre
+    upgrade->spreading_rate=spr_rate;
 }
 
-static void upgrade_destroy(upgrade_t **upgrade){ //détruie le upgrade
+static void upgrade_destroy(upgrade_t **upgrade){ //dï¿½truie le upgrade
     free((*upgrade));
     (*upgrade)=NULL;
 }
 
-static void upgrade_display(upgrade_t *upgrade){ //affiche les données du upgrade
+static void upgrade_display(upgrade_t *upgrade){ //affiche les donnï¿½es du upgrade
     if(upgrade!=NULL){
         printf("Nom du upgrade : %s \n", upgrade->name);
-        printf("Prix : %f \n", *upgrade->price);
-        printf("Taux de propagation : %f \n", *upgrade->spreading_rate);
-        printf("Taux de recherche : %f \n", *upgrade->research_rate);
+        printf("Prix : %f \n", upgrade->price);
+        printf("Taux de propagation : %f \n", upgrade->spreading_rate);
+        printf("Taux de recherche : %f \n", upgrade->research_rate);
     } else {
         printf("Le upgrade n'existe pas");
     }
 }
 
-
+void main(){
+    upgrade_t *op = upgrade_create("kebab",32.5,0.2,0.5);
+    upgrade_display(op);
+    upgrade_edit_rsch_rate(op, 0.5);
+    upgrade_display(op);
+}
 
--- upgrade.h
+++ upgrade.h
@@ -5,9 +5,9 @@ typedef struct
 {
 	char * name;
 
-	float *price;
-	float *research_rate;
-	float *spreading_rate;
+	float price;
+	float research_rate;
+	float spreading_rate;
 } upgrade_t ;
 
 extern upgrade_t * upgrade_create(char name[], float price, float spr_rate, float rsch_rate);
