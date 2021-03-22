de4ce0482b3c4df5483005b4b3ef8d45a779529d
Grégoire Bellon <gregoire.bellon@outlook.fr>
2021-03-22 15:43:30 +0100

    Les chemins relatifs fonctionnent

--- src/botnet.c
+++ src/botnet.c
@@ -12,6 +12,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include <unistd.h>
 
 #include "../lib/virus.h"
 #include "../lib/upgrade.h"
@@ -20,33 +21,33 @@
 #include "../lib/country_list.h"
 #include "../lib/config.h"
 
+void delay(int number_of_seconds)
+{
+    // Converting time into milli_seconds
+    int milli_seconds = 1000 * number_of_seconds;
 
+    // Storing start time
+    clock_t start_time = clock();
 
-void delay(int number_of_seconds) 
-{ 
-    // Converting time into milli_seconds 
-    int milli_seconds = 1000 * number_of_seconds; 
-  
-    // Storing start time 
-    clock_t start_time = clock(); 
-  
-    // looping till required time is not achieved 
-    while (clock() < start_time + milli_seconds) 
-        ; 
-} 
+    // looping till required time is not achieved
+    while (clock() < start_time + milli_seconds)
+        ;
+}
 
 /**
  * @brief Permet d'afficher la liste de tous les pays, utilise la fonction afficher_country
  * @param cl {Pointeur sur une structure de liste des pays}
 */
-void afficher_pays(country_list_t * cl){
-    for(int i=0; i<18; i++){
+void afficher_pays(country_list_t *cl)
+{
+    for (int i = 0; i < 18; i++)
+    {
         afficher_country(cl->liste[i]);
     }
 }
 
-
-void choix(country_list_t * cl, jeu_t * jeu, upgrade_t * upgrade, upgrade_t * cles_usb, upgrade_t * trojan, upgrade_t * fake_ad){
+void choix(country_list_t *cl, jeu_t *jeu, upgrade_t *upgrade, upgrade_t *cles_usb, upgrade_t *trojan, upgrade_t *fake_ad)
+{
     printf(" 1 \n");
     upgrade_display(upgrade);
     printf(" 2 \n");
@@ -61,26 +62,43 @@ void choix(country_list_t * cl, jeu_t * jeu, upgrade_t * upgrade, upgrade_t * cl
     int choix;
     printf("Quel choix voulez vous faire ?");
     scanf("%i", &choix);
-    switch(choix){
-        case 1 : buy_upgrade(jeu, upgrade);
+    switch (choix)
+    {
+    case 1:
+        buy_upgrade(jeu, upgrade);
         break;
-        case 2 : buy_upgrade(jeu, cles_usb);
+    case 2:
+        buy_upgrade(jeu, cles_usb);
         break;
-        case 3 : buy_upgrade(jeu, trojan);
+    case 3:
+        buy_upgrade(jeu, trojan);
         break;
-        case 4 : buy_upgrade(jeu, fake_ad);
+    case 4:
+        buy_upgrade(jeu, fake_ad);
         break;
-        case 5 : afficher_pays(cl);
+    case 5:
+        afficher_pays(cl);
         break;
-        case 6 : exit;
-        default : printf("pouet pouet");
-    } 
+    case 6:
+        exit;
+    default:
+        printf("pouet pouet");
+    }
 }
-
-
 
 int main()
 {
+    int result;
+    char *buf = malloc(200); 
+    result = readlink("/proc/self/exe", buf, 200);
+
+    buf[result] = '\0';
+    char *const last = strrchr(buf, '/');
+    if (last != NULL)
+        *last = '\0';
+
+    chdir(buf);
+    free(buf);
 
     virus_t *virus = virus_create("kaboub", DEFAULT_SPREADING_RATE, DEFAULT_RESEARCH_RATE);
     virus_display(virus);
@@ -95,31 +113,29 @@ int main()
     jeu_t *jeu = jeu_create(virus, DEFAULT_BITCOIN);
     virus_display(jeu->virus);
 
-   /* printf("Thunes : %f \n", jeu->btc);
+    /* printf("Thunes : %f \n", jeu->btc);
     buy_upgrade(jeu, upgrade);
 
     virus_display(jeu->virus);
     printf("Thunes : %f \n", jeu->btc);*/
 
-
-    country_list_t * cl = creer_country_list();
+    country_list_t *cl = creer_country_list();
 
     cl->liste[0]->compromised_pcs_cpt = 10;
-    while(game_state(cl)==0)
+    while (game_state(cl) == 0)
     {
         printf("-----Thunes----- : %f \n", jeu->btc);
         choix(cl, jeu, upgrade, cles_usb, trojan, fake_ad);
-        spread_world(jeu->virus, cl); 
+        spread_world(jeu->virus, cl);
         mine_btc_world(jeu, cl);
-        
     }
-    if(game_state(cl)==1){
+    if (game_state(cl) == 1)
+    {
         printf("Vous avez gagné,wow !");
     }
 
-
     virus_destroy(&jeu->virus);
-    detruire_country_list(&cl); 
+    detruire_country_list(&cl);
 
     return 0;
 }
